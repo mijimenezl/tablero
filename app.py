66d7df784fb916f9ec19915c97eded860fbced92
mijimenezl <167145289+mijimenezl@users.noreply.github.com>
2024-10-03 09:55:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,12 @@ option = st.selectbox(
 )
 
 
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    height=200,
+    width=200,
+    key="canvas",
+)
